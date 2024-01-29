e03ae2b3a803c2a7823ec27c6cd3bd55634a852c
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2024-01-29 14:44:20 -0300

    Deixa a content page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    id: 1,
-    photo: 'aaa',
-    title: 'texto do titulo',
-    description: 'Ola',
+    id: '1',
+    title: 'texto do titulo id 1',
+    description: 'Descrição 1',
+    photoCover: 'https://static.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg',
   },
   {
-    id: 1,
-    photo: 'aaa',
-    title: 'texto do titulo',
-    description: 'Ola',
+    id: '2',
+    title: 'texto do titulo id 2',
+    description: 'Descrição 2',
+    photoCover: 'https://windowsclub.com.br/wp-content/uploads/2022/09/homem-ferro.jpg',
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,13 +8,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = 'https://static.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg';
-  contentTitle: string = 'MINHA NOTÍCIA';
-  contentDescription: string = 'Olá Mundo!';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id: string | null = '0';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(value => console.log(value.get('id')));
+    this.route.paramMap.subscribe(value => (this.id = value.get('id')));
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
   }
 }
